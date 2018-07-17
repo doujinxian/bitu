4c191b241e4949e8e2ffdbff26fb943707f11d65
unknown <jinxian.dou@office.51fanli.com>
2018-07-17 09:00:29 +0800

    trade7

--- parent/bitm-model/src/main/java/com/bitm/model/mapper/TradeMapper.java
+++ parent/bitm-model/src/main/java/com/bitm/model/mapper/TradeMapper.java
@@ -13,19 +13,19 @@ import java.util.List;
 @Mapper
 public interface TradeMapper {
 
-    @Insert("INSERT INTO trade_6(symbol, platform, trade_id, price, qty, is_buyer_maker, TRADE_TIME, CREATE_TIME, UPDATE_TIME ) " +
+    @Insert("INSERT INTO trade_7(symbol, platform, trade_id, price, qty, is_buyer_maker, TRADE_TIME, CREATE_TIME, UPDATE_TIME ) " +
             "VALUES(#{symbol}, #{platform}, #{tradeId}, #{price}, #{qty}, #{isBuyerMaker}, #{tradeTime}, #{createTime}, #{updateTime})")
     int insertTrade(Trade trade);
 
-    @Select("select max(t.`trade_id`) from trade_6 t where t.`symbol` = #{symbol}  and platform=1 ")
+    @Select("select max(t.`trade_id`) from trade_7 t where t.`symbol` = #{symbol}  and platform=1 ")
     Long findMaxTradeIdBySymbol(@Param("symbol") String symbol);
 
-    @Select("SELECT ROUND(SUM(b.`qty`),8) as qty,b.`is_buyer_maker` as isBuyerMaker ,b.`symbol` FROM  trade_6 b WHERE b.`TRADE_TIME`>= #{tradeStartTime} AND b.`TRADE_TIME` < #{tradeEndTime} and platform=1 GROUP BY  b.`symbol`,b.`is_buyer_maker`")
+    @Select("SELECT ROUND(SUM(b.`qty`),8) as qty,b.`is_buyer_maker` as isBuyerMaker ,b.`symbol` FROM  trade_7 b WHERE b.`TRADE_TIME`>= #{tradeStartTime} AND b.`TRADE_TIME` < #{tradeEndTime} and platform=1 GROUP BY  b.`symbol`,b.`is_buyer_maker`")
     List<BiTradeQty> findBiTradeQtyList(@Param("tradeStartTime") Long tradeStartTime, @Param("tradeEndTime") Long tradeEndTime);
 
-    @Select("SELECT b.`qty` FROM  `trade_6` b WHERE b.`symbol` = #{symbol}  and platform=1 limit #{limit}")
+    @Select("SELECT b.`qty` FROM  `trade_7` b WHERE b.`symbol` = #{symbol}  and platform=1 limit #{limit}")
     List<BigDecimal> findQtyList(@Param("symbol") String symbol, @Param("limit") Integer limit);
 
-    @Select("SELECT b.`qty` FROM  `trade_6` b WHERE b.`symbol` = #{symbol} AND b.`TRADE_TIME`>= #{tradeStartTime} AND b.`TRADE_TIME`< #{tradeEndTime} and platform=1 limit 100000")
+    @Select("SELECT b.`qty` FROM  `trade_7` b WHERE b.`symbol` = #{symbol} AND b.`TRADE_TIME`>= #{tradeStartTime} AND b.`TRADE_TIME`< #{tradeEndTime} and platform=1 limit 100000")
     List<BigDecimal> findQtyListInTradeTime(@Param("symbol") String symbol, @Param("tradeStartTime") Long tradeStartTime, @Param("tradeEndTime") Long tradeEndTime);
 }
